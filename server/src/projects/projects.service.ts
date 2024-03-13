f6c2f7185965273ccf53dfe0964a34d5fdf0d344
Dhritiman Das <iamdhritiman01@gmail.com>
2024-03-13 13:04:04 +0530

    fix

--- server/src/projects/projects.service.ts
+++ server/src/projects/projects.service.ts
@@ -344,6 +344,7 @@ export class ProjectsService {
           inviteeEmail: addMembersDto.email,
           projectName: project.name,
           ip: '',
+          inviteeName: '',
         }),
       });
 
